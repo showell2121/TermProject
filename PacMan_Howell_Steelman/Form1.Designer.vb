7a100966720a7ecfe0b7eae24354bcdbdf51fbac
showell2121 <showell2121@gmail.com>
2021-04-01 15:50:59 -0500

    Update Form1.Designer.vb
    added comment

--- PacMan_Howell_Steelman/Form1.Designer.vb
+++ PacMan_Howell_Steelman/Form1.Designer.vb
@@ -1,7 +1,7 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Form1
   Inherits System.Windows.Forms.Form
-
+  'changes from branch1
   'Form overrides dispose to clean up the component list.
   <System.Diagnostics.DebuggerNonUserCode()> _
   Protected Overrides Sub Dispose(ByVal disposing As Boolean)
